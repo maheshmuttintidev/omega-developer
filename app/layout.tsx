0265684e1936049869b486bda4afaee15f53118d
maheshmuttintidev <maheshmuttintidev@gmail.com>
2024-07-12 13:58:59 +0530

    open graph seo optimization part 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,32 @@ export const metadata: Metadata = {
     "Omega Developer is none other than Mahesh Muttinti, He will share the youtube videos related to the Anime, React, React Native, Node Js, MongoDB, Postgresql, and more interesting contents.",
   metadataBase: new URL("https://omage-developer.maheshmuttintidev.in"),
   openGraph: {
-    images: "/opengraph-image.png",
+    title: "Omega Developer - Full Stack Web Developer",
+    description:
+      "Omega Developer is none other than Mahesh Muttinti, He will share the youtube videos related to the Anime, React, React Native, Node Js, MongoDB, Postgresql, and more interesting contents.",
+    url: "https://maheshmuttintidev.in",
+    siteName: "Mahesh Muttinti - Full Stack Web & Mobile App Developer",
+    locale: "en",
+    type: "website",
+    images: ["https://omega-developer.maheshmuttintidev.in/banner.png"],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Omega Developer - Full Stack Web Developer",
+    description:
+      "Omega Developer is none other than Mahesh Muttinti, He will share the youtube videos related to the Anime, React, React Native, Node Js, MongoDB, Postgresql, and more interesting contents.",
+    creator: "@MaheshMuttinti",
+    images: ["https://omega-developer.maheshmuttintidev.in/banner.png"],
+  },
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      new URL("/favicon.ico", "https://omega-developer.maheshmuttintidev.in"),
+    ],
+    apple: [{ url: "/apple-icon.png" }],
+  },
+  alternates: {
+    canonical: "/",
   },
 };
 
