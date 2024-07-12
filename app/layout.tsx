112e6724c7e5a37886f580ff5ad84c9553f4428f
maheshmuttintidev <maheshmuttintidev@gmail.com>
2024-07-12 23:04:58 +0530

    fixed opengraph json - test 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,24 +9,6 @@ export const metadata: Metadata = {
   description:
     "Omega Developer is none other than Mahesh Muttinti, He will share the youtube videos related to the Anime, React, React Native, Node Js, MongoDB, Postgresql, and more interesting contents.",
   metadataBase: new URL("https://omage-developer.maheshmuttintidev.in"),
-  openGraph: {
-    title: "Omega Developer - Full Stack Web Developer",
-    description:
-      "Omega Developer is none other than Mahesh Muttinti, He will share the youtube videos related to the Anime, React, React Native, Node Js, MongoDB, Postgresql, and more interesting contents.",
-    url: "https://maheshmuttintidev.in",
-    siteName: "Mahesh Muttinti - Full Stack Web & Mobile App Developer",
-    locale: "en",
-    type: "website",
-    images: ["https://omega-developer.maheshmuttintidev.in/banner.png"],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Omega Developer - Full Stack Web Developer",
-    description:
-      "Omega Developer is none other than Mahesh Muttinti, He will share the youtube videos related to the Anime, React, React Native, Node Js, MongoDB, Postgresql, and more interesting contents.",
-    creator: "@MaheshMuttinti",
-    images: ["https://omega-developer.maheshmuttintidev.in/banner.png"],
-  },
   icons: {
     icon: [
       { url: "/favicon.ico" },
@@ -35,7 +17,39 @@ export const metadata: Metadata = {
     apple: [{ url: "/apple-icon.png" }],
   },
   alternates: {
-    canonical: "/",
+    canonical: "https://omage-developer.maheshmuttintidev.in",
+  },
+  openGraph: {
+    title: "Omega Developer - Full Stack Web Developer",
+    description:
+      "Omega Developer is none other than Mahesh Muttinti, He will share the youtube videos related to the Anime, React, React Native, Node Js, MongoDB, Postgresql, and more interesting contents.",
+    url: "https://omega-developer.maheshmuttintidev.in",
+    siteName: "Mahesh Muttinti - Full Stack Web & Mobile App Developer",
+    locale: "en",
+    type: "website",
+    images: [
+      {
+        url: "https://omega-developer.maheshmuttintidev.in/banner.png",
+        width: 1920,
+        height: 1080,
+        alt: "Banner Image",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Omega Developer - Full Stack Web Developer",
+    description:
+      "Omega Developer is none other than Mahesh Muttinti, He will share the youtube videos related to the Anime, React, React Native, Node Js, MongoDB, Postgresql, and more interesting contents.",
+    creator: "@MaheshMuttinti",
+    images: [
+      {
+        url: "https://omega-developer.maheshmuttintidev.in/banner.png",
+        width: 1920,
+        height: 1080,
+        alt: "Banner Image",
+      },
+    ],
   },
 };
 
@@ -46,6 +60,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="text">
+      <head>
+        <meta
+          name="robots"
+          content="max-snippet:-1, max-image-preview:large, max-video-preview:-1"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
